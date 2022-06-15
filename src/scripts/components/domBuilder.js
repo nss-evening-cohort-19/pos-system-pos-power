3b3cdea4be967fbd7cb31eca99108b3dfea5d9e1
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-06-14 19:24:18 -0500

    add domBuilder

---
+++ src/scripts/components/domBuilder.js
@@ -0,0 +1,14 @@
+import renderToDOM from '../helpers/renderToDom';
+
+const domBuilder = () => {
+  const domString = `
+  <div id="navigation"></div>
+  <div id="main-container">
+    <div id="form-container"></div>
+    <div id="view"></div>
+  </div>`;
+
+  renderToDOM('#app', domString);
+};
+
+export default domBuilder;
