2849ceeb7ec053fee20ca1155baf15499da7749b
Christian Black <blackl3@gmail.com>
2022-06-14 21:24:13 -0500

    created order call to get orders from database and display on DOM

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,11 +1,14 @@
+import getAllOrders from '../../api/ordersData';
 import domBuilder from '../components/domBuilder';
 import logoutButton from '../components/logoutButton';
 import navBar from '../components/navBar';
+import renderOrders from '../components/pages/orders';
 
 const startApp = () => {
   domBuilder();
   navBar();
   logoutButton();
+  getAllOrders().then((response) => renderOrders(response));
 };
 
 export default startApp;
