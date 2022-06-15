306a459fd8ba56742d551e984ed7746c3a56a7ba
Mitch McCullough <mitchmccullough@gmail.com>
2022-06-14 21:00:43 -0500

    added loginscreen

--- src/scripts/components/loginButton.js
+++ src/scripts/components/loginButton.js
@@ -1,8 +1,13 @@
 import signIn from '../helpers/signIn';
+import logo from '../../../instructions/hhpw-record.png';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  document.querySelector('#app').innerHTML = '';
+  const domString = `
+  <img src=${logo} class="logo">
+  <button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>
+  `;
   document.querySelector('#login-form-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
