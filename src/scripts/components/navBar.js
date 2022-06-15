20c031dbce0c1f053301fc0b21aae5d670a7e965
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-06-15 12:46:52 -0500

    fixed and formatted navBar

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -1,17 +1,18 @@
 import renderToDom from '../helpers/renderToDom';
+import logo from '../../../instructions/hhpw-record.png';
 
 const navBar = () => {
   const domString = `
-    <nav id="navBarDiv" class="navbar navbar-expand-lg bg-light">
+  <nav id="navBarDiv" class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-      <span class="icon" style="span" width:50px></span>
-      <a class="navbar-brand" href="#">
-      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav">
+          <li id="logo-image">
+            <img id="navLogo" src=${logo}
+          </li>
           <li class="nav-item">
             <a id="view-order" class="nav-link" aria-current="page" href="#">View Order</a>
           </li>
@@ -19,14 +20,14 @@ const navBar = () => {
             <a id="create-order" class="nav-link" href="#">Create an Order</a>
           </li>
         </ul>
-        <div id="right-nav">
-          <div class="nav-item">
-            <input type="text" class="form-control" id="searchBar-input" placeholder="Search Orders">
-          </div>
-          <div class="nav-item">
+        <ul class="navbar-nav ms-auto">
+          <form id="search-bar-div" class="nav-item">
+            <input type="search" class="form-control" id="searchBar-input" placeholder="Search Orders">
+          </form>
+          <div id="logout-nav-div" class="nav-link">
             <div id="logout-nav">Logout</div>
           </div>
-        </div>
+        </ul>
       </div>
     </div>
   </nav>
