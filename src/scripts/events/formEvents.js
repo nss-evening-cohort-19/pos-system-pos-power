a7d618f72c64c8a5c8af2e207cda020371b28b37
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-06-17 12:10:12 -0500

    added edit functionality to order form.  Pull down and npm start to test.

--- src/scripts/events/formEvents.js
+++ src/scripts/events/formEvents.js
@@ -1,4 +1,4 @@
-import { createOrder } from '../../api/ordersData';
+import { createOrder, updateOrder } from '../../api/ordersData';
 import renderOrders from '../components/pages/orders';
 
 const formEvents = (uid) => {
@@ -14,6 +14,20 @@ const formEvents = (uid) => {
         uid,
       };
       createOrder(newOrder).then((response) => renderOrders(response));
+    }
+
+    if (e.target.id.includes('update-order')) {
+      const [, firebaseKey] = e.target.id.split('--');
+      const updatedOrder = {
+        customerEmail: document.querySelector('#customerEmail').value,
+        customerPhoneNumber: document.querySelector('#customerPhoneNumber').value,
+        last_name: document.querySelector('#last_name').value,
+        orderStatus: 'open',
+        orderType: document.querySelector('#orderType').value,
+        firebaseKey,
+        uid
+      };
+      updateOrder(updatedOrder).then((orderArray) => renderOrders(orderArray));
     }
   });
 };
