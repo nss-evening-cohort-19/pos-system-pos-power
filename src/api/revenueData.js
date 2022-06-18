c9afa3dcf9fe2ac146856434a0db2913918ce563
Christian Black <blackl3@gmail.com>
2022-06-18 09:25:36 -0500

    merged new main and commented out non-functional revenue data function

--- src/api/revenueData.js
+++ src/api/revenueData.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import firebaseConfig from './apiKeys';
-import revenueObj from './revenueDataFunctions';
+// import revenueObj from './revenueDataFunctions';
 
 const dbURL = firebaseConfig.databaseURL;
 
 const getAllRevenueObj = () => new Promise((resolve, reject) => {
   axios.get(`${dbURL}/revenue.json`)
-    .then((response) => ((Object.values(response.data))))
-    .then((responseArr) => resolve(console.warn(revenueObj(responseArr))))
+    .then((response) => (console.warn((Object.values(response.data)))))
+    // .then((responseArr) => resolve(console.warn(revenueObj(responseArr))))
     .catch((reject));
 });
 
