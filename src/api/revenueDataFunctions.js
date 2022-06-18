60bf02873ac356ef620f0d1cb673626f411dc184
Christian Black <blackl3@gmail.com>
2022-06-18 13:37:47 -0500

    Got date range to work

--- src/api/revenueDataFunctions.js
+++ src/api/revenueDataFunctions.js
@@ -24,8 +24,28 @@ const getPaymentTypeTotal = (arr, typeOfOrder) => {
   return filteredArr.length;
 };
 
+const getDateRange = (arr, dateDesired) => {
+  let date = null;
+  let finalDate = null;
+  const sortedArr = arr.sort((a, b) => {
+    const da = new Date(a.date);
+    const db = new Date(b.date);
+    return da - db;
+  });
+  if (dateDesired === 'oldest') {
+    date = new Date(sortedArr[0].date);
+    finalDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+  } else if (dateDesired === 'newest') {
+    date = new Date(sortedArr[arr.length - 1].date);
+    finalDate = `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+  }
+  return finalDate;
+};
+
 const revenueObj = (arr) => {
   const newRevenueObj = {
+    earlyDate: getDateRange(arr, 'oldest'),
+    lateDate: getDateRange(arr, 'newest'),
     totalAmount: getTotalRevenue(arr),
     tipAmount: getTotalTips(arr),
     walkInOrders: getOrderTypeTotal(arr, 'walk-in'),
