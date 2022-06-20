f630f45535409229c58efbd6adb87825cf4d8787
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-06-19 19:13:19 -0500

    fixed bug- styling on orderDetails page finished. buttons and cards styled. responsive window. hover effect on buttons.

--- src/scripts/components/pages/orderDetails.js
+++ src/scripts/components/pages/orderDetails.js
@@ -8,14 +8,13 @@ const orderDetails = (obj = {}) => {
     let domString = '<div id="itemsContainer">';
     itemArray.forEach((card) => {
       domString += `
-      <div style="width: 18 rem;" class="items-div">
-        <div "width: 18 rem;" class="card-body items-card">
+      <div style="width: 25rem;">
+        <div class="card-body order-card">
           <h5 class="card-title">${card.item_name}</h5>
           <h6 class="card-subtitle mb-2 text-muted">${card.item_price}</h6>
           <button class="btn btn-primary" id="edit-item-btn--${card.firebaseKey}">Edit</button>
           <button class="btn btn-danger" id="delete-item-btn--${card.firebaseKey}">Delete</button>
         </div>
-      </div>
     </div>`;
     });
     domString += `
@@ -29,14 +28,10 @@ const orderDetails = (obj = {}) => {
     renderToDOM('#details-buttons', buttonString);
   } else {
     const htmlString = `
-    <h2>No Items in Order</h2>
-    <div id=orderDetailsButtons>
-      <button id="addItemButton" class="btn btn-success addItemBtn">Add Item</button>
-      <button id="goToPaymentButton" class="btn btn-primary goToPaymentBtn">Go To Payment</button>
-    </div>`;
+    <h2 id="no-items">No Items in Order</h2>`;
     const buttonString = `
-    <div id=orderDetailsButtons>
-      <button id="addItemButton--${obj.firebaseKey}" class="btn btn-success addItemBtn">Add Item</button>
+    <div id=orderDetailsButtons-empty>
+      <button id="addItemButton" class="btn btn-success addItemBtn">Add Item</button>
       <button id="goToPaymentButton" class="btn btn-primary goToPaymentBtn">Go To Payment</button>
     </div>`;
     renderToDOM('#view', htmlString);
