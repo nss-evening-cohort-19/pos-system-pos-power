a42dc3cef5afdf8613c824adc323c7c200e7cf3b
Mitch McCullough <mitchmccullough@gmail.com>
2022-06-20 13:18:42 -0500

    quick fix to add items to empty orders

--- src/scripts/components/pages/orderDetails.js
+++ src/scripts/components/pages/orderDetails.js
@@ -28,7 +28,7 @@ const orderDetails = (obj = {}) => {
     const htmlString = `
     <h2>No Items in Order</h2>
     <div id=orderDetailsButtons>
-      <button id="addItemButton" class="btn btn-success addItemBtn">Add Item</button>
+      <button id="addItemButton--${obj.firebaseKey}" class="btn btn-success addItemBtn">Add Item</button>
       <button id="goToPaymentButton" class="btn btn-primary goToPaymentBtn">Go To Payment</button>
     </div>`;
     renderToDOM('#view', htmlString);
