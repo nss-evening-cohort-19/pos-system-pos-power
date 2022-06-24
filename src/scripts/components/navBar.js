f1f88e6f9d2f649c081a91f59cbaca42f18e12e0
Christian Black <blackl3@gmail.com>
2022-06-24 12:46:10 -0500

    saving changes

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -5,7 +5,7 @@ const navBar = () => {
   const domString = `
   <nav id="navBarDiv" class="navbar navbar-expand-lg navbar-dark mb-5">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navBarToggleBtn">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navBarToggleBtn">
         <span class="navbar-toggler-icon"></span>
       </button>
       <a id="logo-image" class="navbar-brand">
