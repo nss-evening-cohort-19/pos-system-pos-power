a81049a21f669d2898d3a12d4748e6d89976b16a
Christian Black <blackl3@gmail.com>
2022-06-23 19:52:22 -0500

    merging new dev

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -5,34 +5,35 @@ const navBar = () => {
   const domString = `
   <nav id="navBarDiv" class="navbar navbar-expand-lg navbar-dark mb-5">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navbarToggleBtn">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="navBarToggleBtn">
         <span class="navbar-toggler-icon"></span>
       </button>
       <a id="logo-image" class="navbar-brand">
         <img id="navLogo" src="${logo}">
       </a>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-collapse collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a id="home-nav" class="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" aria-current="page" href="#">Home</a>
+            <a id="home-nav" class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a id="view-order" class="nav-link" data-bs-toggle="collapse"  data-bs-target=".navbar-collapse.show" aria-current="page" href="#">View Orders</a>
+            <a id="view-order" class="nav-link" aria-current="page" href="#">View Orders</a>
           </li>
           <li class="nav-item">
-            <a id="create-order" class="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" href="#">Create an Order</a>
-          </li>
+            <a id="create-order" class="nav-link" href="#">Create an Order</a>
           </li>
           <li class="nav-item">
-            <a id="menu-nav" class="nav-link" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show" href="#">Menu</a>
+            <a id="menu-nav" class="nav-link" href="#">Menu</a>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto">
-          <form id="search-bar-div" class="nav-item" onsubmit="return false" data-bs-toggle="collapse">
+          <form id="search-bar-div" class="nav-item" onsubmit="return false">
             <input type="search" class="form-control" id="searchBar-input" placeholder="Search Orders">
           </form>
           <div id="logout-nav-div" class="nav-link">
-            <div id="logout-nav">Logout</div>
+            <div id="logout-nav">
+              Logout
+            </div>
           </div>
         </ul>
       </div>
